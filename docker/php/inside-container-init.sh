bd5896bd5c4d3ec22382a51e83f51e5ece440671
Diogo Coutinho <diogo.coutinho@odgo.com.br>
2025-08-15 23:05:15 -0300

    Refactor inside-container-init.sh to update directory references for Laravel and Hyperf installations, ensuring correct path handling by using APP_DIR and TARGET_DIR, and improving setup reliability.

--- docker/php/inside-container-init.sh
+++ docker/php/inside-container-init.sh
@@ -8,21 +8,18 @@ bash /usr/local/bin/setup-framework.sh "$FW"
 
 TARGET_DIR="/var/www/html"
 
-FW_DIR="$TARGET_DIR/$FW"
+FW_DIR="$APP_DIR/$FW"
 
 if [ "$FW" = "laravel" ]; then
-  if [ -f "$FW_DIR/composer.json" ]; then
-    echo "🚨composer.json found, running composer install..."
-    echo "Listando arquivos de $FW_DIR:"
-    ls -la "$FW_DIR"
-    cd "$FW_DIR"
+  if [ -f "$TARGET_DIR/composer.json" ]; then
+    echo "composer.json found, running composer install..."
     composer install --no-interaction --prefer-dist --optimize-autoloader
   else
-    if [ "$(ls -A $FW_DIR)" ]; then
-      echo "Directory $FW_DIR is not empty but composer.json not found, skipping create-project."
+    if [ "$(ls -A $TARGET_DIR)" ]; then
+      echo "Directory $TARGET_DIR is not empty but composer.json not found, skipping create-project."
     else
-      echo "Creating new Laravel project in $FW_DIR..."
-      composer create-project --prefer-dist laravel/laravel "$FW_DIR"
+      echo "Creating new Laravel project in $TARGET_DIR..."
+      composer create-project --prefer-dist laravel/laravel "$TARGET_DIR"
     fi
   fi
 
@@ -34,11 +31,11 @@ if [ "$FW" = "laravel" ]; then
   fi
 
   # Set permissions
-  chown -R www-data:www-data "$FW_DIR"
-  chmod -R 755 "$FW_DIR"
+  chown -R www-data:www-data "$TARGET_DIR"
+  chmod -R 755 "$TARGET_DIR"
 
 elif [ "$FW" = "hyperf" ]; then
-  if [ ! -f "$FW_DIR/composer.json" ]; then
-    composer create-project hyperf/hyperf-skeleton "$FW_DIR"
+  if [ ! -f "$TARGET_DIR/composer.json" ]; then
+    composer create-project hyperf/hyperf-skeleton "$TARGET_DIR"
   fi
 fi
